20142d21040c3c656967da4fca8b349173ccac85
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2023-10-26 19:22:41 -0300

    Rodapé da página

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    <h2>Footer</h2>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "FooterPlace",
+});
+</script>
+<style>
+.footer {
+  height: 60px;
+  background-color: red;
+}
+</style>
