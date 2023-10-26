7aa9d2e8ef9b1e4e88293f3eeab5c5ada60587da
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2023-10-26 19:09:07 -0300

    Aba dos produtos do e-commerce

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <ContentPlace />
+  <FooterPlace />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import ContentPlace from "@/components/main/Content.vue";
+import FooterPlace from "@/components/footer/Footer.vue";
+export default defineComponent({
+  name: "ProductsView",
+  components: {
+    ContentPlace,
+    FooterPlace,
+  },
+});
+</script>
