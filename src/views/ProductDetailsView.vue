1571626e6b1f46907e153655aeea41c4277aff88
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2023-10-29 21:13:39 -0300

    Página dos detalhes sobre o produto

---
+++ src/views/ProductDetailsView.vue
@@ -0,0 +1,14 @@
+<template>
+  <ProductDetails />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import ProductDetails from "./ProductsView.vue";
+export default defineComponent({
+  name: "ProductDetailsView",
+  components: {
+    ProductDetails,
+  },
+});
+</script>
+<style></style>
