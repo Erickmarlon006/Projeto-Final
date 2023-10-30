131e803654726f71726d99a27d645809e359c299
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2023-10-29 21:16:06 -0300

    Componente que fornece os detalhes do produto

---
+++ src/components/Details.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="content">
+    <h2>Essa é a página dos detalhes</h2>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+//import EventBus from "@/services/event-bus.js";
+export default defineComponent({
+  name: "ProductDetails",
+  data() {
+    return {
+      itemDetailsId: "",
+      itemDetails: null,
+    };
+  },
+  created() {
+    /*/
+      EventBus.$on("selectedId", (testeId: string) => {
+      this.itemDetailsId = testeId;
+      this.getItemDetails(this.itemDetailsId);
+      console.log(this.itemDetailsId);
+    });
+    /*/
+  },
+  methods: {
+    async getItemDetails(itemId: string) {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/items/${itemId}`
+        );
+        this.itemDetails = response.data;
+      } catch (error) {
+        console.error("Erro ao buscar os detalhes do item:" + error);
+      }
+    },
+  },
+});
+</script>
