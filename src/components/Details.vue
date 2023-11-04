682604bef9314ee55a0c114deb913a0428d756f5
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2023-11-04 13:15:22 -0300

    Componente que contém os detalhes do item

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,39 +1,49 @@
 <template>
   <div class="content">
     <h2>Essa é a página dos detalhes</h2>
+    <div v-if="itemDetails.length">
+      <div v-for="item in itemDetails" :key="item.id" class="item-card">
+        <h3>{{ item.title }}</h3>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import axios from "axios";
-//import EventBus from "@/services/event-bus.js";
+import { emitter } from "@/services/event-bus";
 export default defineComponent({
   name: "ProductDetails",
   data() {
     return {
       itemDetailsId: "",
-      itemDetails: null,
+      itemsList: [],
+      itemDetails: [] as {
+        title: string;
+        price: number;
+        img: string;
+        id: string;
+      }[],
     };
   },
-  created() {
-    /*/
-      EventBus.$on("selectedId", (testeId: string) => {
-      this.itemDetailsId = testeId;
-      this.getItemDetails(this.itemDetailsId);
-      console.log(this.itemDetailsId);
-    });
-    /*/
+  async mounted() {
+    this.itemsList = await axios
+      .get("http://localhost:3000/items/")
+      .then((resp) => resp.data)
+      .catch((error) => console.log(error));
+    this.getItemId();
   },
   methods: {
-    async getItemDetails(itemId: string) {
-      try {
-        const response = await axios.get(
-          `http://localhost:3000/items/${itemId}`
-        );
-        this.itemDetails = response.data;
-      } catch (error) {
-        console.error("Erro ao buscar os detalhes do item:" + error);
-      }
+    async getItemId() {
+      emitter.on("selectedId", async (id: string) => {
+        this.itemDetailsId = id;
+        await this.getItemDetails(this.itemDetailsId);
+      });
+    },
+    async getItemDetails(id: string) {
+      console.log(id);
+      this.itemDetails = this.itemsList.filter((item) => item["id"] === id);
+      console.log(this.itemDetails);
     },
   },
 });
